5ce868532cc9e5c57c58cf2ada9ac023e4821d6a
maxwen <max.weninger@gmail.com>
2017-10-09 05:52:22 +0000

    base: allow hiding lockscreen shortcuts on secure lockscreen [1/2]
    * Cardinal edits:
      split Disable QS and Hide Lockscreen shortcuts on secure lockscreen into two seperate options
    
    * Extracted from omnirom/android_frameworks_base@1e89b4f

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardBottomAreaView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardBottomAreaView.java
@@ -54,6 +54,7 @@ import android.os.Messenger;
 import android.os.RemoteException;
 import android.os.UserHandle;
 import android.provider.MediaStore;
+import android.provider.Settings;
 import android.service.media.CameraPrewarmService;
 import android.service.quickaccesswallet.GetWalletCardsError;
 import android.service.quickaccesswallet.GetWalletCardsResponse;
@@ -1007,7 +1008,7 @@ public class KeyguardBottomAreaView extends FrameLayout implements View.OnClickL
         public IconState getIcon() {
             mLeftIsVoiceAssist = canLaunchVoiceAssist();
             if (mLeftIsVoiceAssist) {
-                mIconState.isVisible = mUserSetupComplete && mShowLeftAffordance;
+                mIconState.isVisible = mUserSetupComplete && mShowLeftAffordance && !hideShortcuts();
                 if (mLeftAssistIcon == null) {
                     mIconState.drawable = mContext.getDrawable(R.drawable.ic_mic_26dp);
                 } else {
@@ -1017,7 +1018,7 @@ public class KeyguardBottomAreaView extends FrameLayout implements View.OnClickL
                         R.string.accessibility_voice_assist_button);
             } else {
                 mIconState.isVisible = mUserSetupComplete && mShowLeftAffordance
-                        && isPhoneVisible();
+                        && isPhoneVisible() && !hideShortcuts();
                 mIconState.drawable = mContext.getDrawable(
                         com.android.internal.R.drawable.ic_phone);
                 mIconState.contentDescription = mContext.getString(
@@ -1042,7 +1043,8 @@ public class KeyguardBottomAreaView extends FrameLayout implements View.OnClickL
             mIconState.isVisible = !isCameraDisabled
                     && mShowCameraAffordance
                     && mUserSetupComplete
-                    && resolveCameraIntent(0) != null;
+                    && resolveCameraIntent(0) != null
+                    && !hideShortcuts();
             mIconState.drawable = mContext.getDrawable(R.drawable.ic_camera_alt_24dp);
             mIconState.contentDescription =
                     mContext.getString(R.string.accessibility_camera_button);
@@ -1099,6 +1101,13 @@ public class KeyguardBottomAreaView extends FrameLayout implements View.OnClickL
                 com.android.internal.R.attr.colorSurface);
         mWalletButton.setBackgroundTintList(bgColor);
         mControlsButton.setBackgroundTintList(bgColor);
+    }
+
+     private boolean hideShortcuts() {
+        boolean secure = mKeyguardStateController.isMethodSecure();
+        return secure && Settings.Secure.getIntForUser(
+                mContext.getContentResolver(), Settings.Secure.HIDE_LOCK_SHORTCUTS, 0,
+                KeyguardUpdateMonitor.getCurrentUser()) != 0;
     }
 
     /**
